e0cd0ab866235e01dcb16e7ba015c79443c021af
Guel.x <ml2338668@gmail.com>
2024-02-02 09:51:34 -0300

    Criada a classe PossicaoXadrez

---
+++ xadrez-console/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,20 @@
+﻿namespace Xadrez
+{
+    class PosicaoXadrez
+    {
+
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez()
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+
+        public override string ToString()
+        {
+            return "" + coluna + linha;
+        }
+    }
+}
